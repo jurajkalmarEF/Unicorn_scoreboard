d7bf5c84cf3d70afe81a1c5de7f53b0361e44b4e
jurajkalmarEF <juraj.kalmar@gmail.com>
2026-09-03 17:14:28 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,4 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("org.json:json:20240303")
 }
